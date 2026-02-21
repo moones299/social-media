161438bb20ff987bf4d9cceb7a490b02c64695a6
Mohammad Reza <attarmohammadreza83@gmail.com>
2026-02-21 21:24:25 +0330

    Frontend-preparation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p className="text-red-700">dsadad</p>
+    </div>
+  );
+}
+
+export default App;
